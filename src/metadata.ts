74cccb42edbbf4a2b3cf07ff11aa04ac40712763
Terran Blake <terranblake@gmail.com>
2019-12-25 17:09:37 -0800

    fix metadata endpoint module using pluralize

--- src/metadata.ts
+++ src/metadata.ts
@@ -3,6 +3,7 @@ import { Model } from 'mongoose';
 import * as config from 'config';
 import { promisify } from 'util';
 
+const pluralize = require('pluralize');
 const request = require('request');
 const requestAsync = promisify(request);
 
@@ -11,10 +12,11 @@ export default async (model: Model<any, {}>, ticker: string, accessionNumber: st
         throw new Error('no model provided. unable to retrieve metadata for a missing model');
     }
 
+    const modelName = String(pluralize(model.modelName)).toLowerCase();
     const metadataService = config.has('metadata-service.base') || 'http://localhost:5000';
 
     accessionNumber = accessionNumber && `&accessionNumber=${accessionNumber}` || '';
-    const url = `${metadataService}/${model.modelName}?ticker=${ticker}${accessionNumber}`;
+    const url = `${metadataService}/${modelName}?ticker=${ticker}${accessionNumber}`;
 
     const { body = {} } = await requestAsync({ method: 'GET', url, json: true });
     return body;
